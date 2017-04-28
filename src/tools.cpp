d147455e7704aad2d52087a53c512d708f3d58d1
Tim Abrahamsen <timmy_neutron@mac.com>
2017-04-27 20:16:55 -0400

    Complete CalculateRMSE function

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,28 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+	// initialize
+	VectorXd rmse = VectorXd::Zero(4);
+	VectorXd residual = VectorXd(4);
+	int n_estimations = estimations.size();
+
+	for (int i=0; i<n_estimations; i++) {
+		// calculate residual
+		residual = estimations[i] - ground_truth[i];
+
+		// square residual
+		residual = residual.array() * residual.array();
+
+		// sum residuals
+		rmse += residual;
+	}
+
+	// find mean of squared residuals
+	rmse /= n_estimations;
+
+	// take square root of mean
+	rmse = rmse.array().sqrt();
+
+	// return rmse
+	return rmse;
 }
